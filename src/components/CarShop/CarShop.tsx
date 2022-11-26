2bd96beab8391c5d4ec7093eb53f068baeb8fdd4
Lada Penkova <whitethe2001@gmail.com>
2022-11-26 16:21:59 +0200

    Searching cars

--- src/components/CarShop/CarShop.tsx
+++ src/components/CarShop/CarShop.tsx
@@ -1,17 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { CARS } from "../../helpers/const";
 
 import CarItem from "../CarItem/CarItem";
 
-import classes from "../../styles/basic/ModalWrapper.module.scss";
+import classes from "../../styles/basic/BasicStyles.module.scss";
 
 const CarShop: React.FC = () => {
+  const [searchValue, setSearchValue] = useState("");
+
+  const searchedCars = CARS.filter(
+    (car) =>
+      car.name.toLowerCase().includes(searchValue) ||
+      car.model.toLowerCase().includes(searchValue)
+  );
+
   return (
     <>
       <div className="container">
         <div className={classes.ModalWrapper}>
-          {CARS.map((item) => (
+          <div className={classes.InputContainer}>
+            <label
+              className={`${classes.InputContainer__label} ${classes.InputContainer__label_icon}`}
+            >
+              <input
+                type="text"
+                placeholder="Search"
+                onChange={(e) => setSearchValue(e.target.value)}
+                value={searchValue}
+              />
+            </label>
+          </div>
+          {searchedCars.map((item) => (
             <CarItem
               key={`${item.id}_${item.name}_${item.model}`}
               option={item}
